f356d4cc83f492ac3d5d936b6357af5b46bc5e85
GoldFrosch <goldfrosch@daum.net>
2025-06-08 00:47:24 +0900

    feat: 캐릭터 애니메이션 세팅

---
+++ Source/ProjectH/Characters/BaseCharacterAnimInstance.cpp
@@ -0,0 +1,39 @@
+﻿#include "BaseCharacterAnimInstance.h"
+
+#include "Player/PlayerCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UBaseCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	Player = Cast<APlayerCharacter>(GetOwningActor());
+	if (!Player)
+	{
+		return;
+	}
+	
+	MovementComponent = Player->GetCharacterMovement();
+}
+
+void UBaseCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!Player || !MovementComponent)
+	{
+		return;
+	}
+
+	GroundSpeed_H = FVector::DotProduct(Player->GetVelocity(), Player->GetActorRightVector());
+	GroundSpeed_V = FVector::DotProduct(Player->GetVelocity(), Player->GetActorForwardVector());
+
+	UE_LOG(LogTemp, Display, TEXT("Horizion: %f, Vertical: %f"), GroundSpeed_H, GroundSpeed_V);
+
+	if (GroundSpeed_H != 0 || GroundSpeed_V != 0)
+	{
+		bShouldMove = MovementComponent->GetCurrentAcceleration() != FVector::ZeroVector;
+	}
+
+	bIsFalling = MovementComponent->IsFalling();
+}
---
+++ Source/ProjectH/Characters/BaseCharacterAnimInstance.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ProjectH/Utils/MacroUtil.h"
+#include "BaseCharacterAnimInstance.generated.h"
+
+class APlayerCharacter;
+class UCharacterMovementComponent;
+
+UCLASS()
+class PROJECTH_API UBaseCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float GroundSpeed_H;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float GroundSpeed_V;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector Velocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bShouldMove;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsFalling;
+	
+protected:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<APlayerCharacter> Player;
+
+	UPROPERTY()
+	TObjectPtr<UCharacterMovementComponent> MovementComponent;
+};
