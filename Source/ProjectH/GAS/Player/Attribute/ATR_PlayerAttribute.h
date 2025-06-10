42dcfa45a5e212174f7c56b5f5cb55abda88bcf2
GoldFrosch <goldfrosch@daum.net>
2025-06-11 00:36:10 +0900

    feat: 플레이어 스텟 Attribute 세팅

---
+++ Source/ProjectH/GAS/Player/Attribute/ATR_PlayerAttribute.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectH/GAS/_Common/Attribute/ATR_BaseAttribute.h"
+#include "ATR_PlayerAttribute.generated.h"
+
+UCLASS()
+class PROJECTH_API UATR_PlayerAttribute : public UATR_BaseAttribute
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, Category="Attributes"
+		, ReplicatedUsing = OnRepMana)
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UATR_PlayerAttribute, Mana)
+
+	UPROPERTY(BlueprintReadOnly, Category="Attributes"
+		, ReplicatedUsing = OnRepMaxMana)
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UATR_PlayerAttribute, MaxMana)
+	
+	UPROPERTY(BlueprintReadOnly, Category="Attributes"
+		, ReplicatedUsing = OnRepStamina)
+	FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS(UATR_PlayerAttribute, Stamina)
+
+	UPROPERTY(BlueprintReadOnly, Category="Attributes"
+		, ReplicatedUsing = OnRepMaxStamina)
+	FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(UATR_PlayerAttribute, MaxStamina)
+	
+protected:
+	virtual void GetLifetimeReplicatedProps(
+		TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	
+	UFUNCTION()
+	virtual void OnRepMana(const FGameplayAttributeData& OldMana);
+	
+	UFUNCTION()
+	virtual void OnRepMaxMana(const FGameplayAttributeData& OldMaxMana);
+	
+	UFUNCTION()
+	virtual void OnRepStamina(const FGameplayAttributeData& OldStamina);
+	
+	UFUNCTION()
+	virtual void OnRepMaxStamina(const FGameplayAttributeData& OldMaxStamina);
+};
---
+++ Source/ProjectH/GAS/_Common/Attribute/ATR_BaseAttribute.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "ATR_BaseAttribute.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+UCLASS()
+class PROJECTH_API UATR_BaseAttribute : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, Category="Attributes"
+		, ReplicatedUsing = OnRepHealth)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UATR_BaseAttribute, Health)
+
+	UPROPERTY(BlueprintReadOnly, Category="Attributes"
+		, ReplicatedUsing = OnRepMaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UATR_BaseAttribute, MaxHealth)
+	
+	UPROPERTY(BlueprintReadOnly, Category="Attributes"
+		, ReplicatedUsing = OnRepMovementSpeed)
+	FGameplayAttributeData MovementSpeed;
+	ATTRIBUTE_ACCESSORS(UATR_BaseAttribute, MovementSpeed)
+
+protected:
+	virtual void GetLifetimeReplicatedProps(
+		TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	
+	UFUNCTION()
+	virtual void OnRepHealth(const FGameplayAttributeData& OldHealth);
+	
+	UFUNCTION()
+	virtual void OnRepMaxHealth(const FGameplayAttributeData& OldMaxHealth);
+	
+	UFUNCTION()
+	virtual void OnRepMovementSpeed(const FGameplayAttributeData& OldMovementSpeed);
+};
