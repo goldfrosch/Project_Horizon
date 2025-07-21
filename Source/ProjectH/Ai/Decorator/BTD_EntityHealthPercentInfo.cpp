7909a7ab68923df97daddbebfb0160f69a1a0228
GoldFrosch <goldfrosch@daum.net>
2025-07-21 22:33:44 +0900

    feat: 공통 엔티티 체력 검증 로직 Decorator 추가

---
+++ Source/ProjectH/Ai/Decorator/BTD_EntityHealthPercentInfo.cpp
@@ -0,0 +1,75 @@
+﻿#include "BTD_EntityHealthPercentInfo.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "AIController.h"
+#include "ProjectH/GAS/_Common/HorizonAbilitySystemComponent.h"
+#include "ProjectH/GAS/_Common/Attribute/ATR_BaseAttribute.h"
+
+UBTD_EntityHealthPercentInfo::UBTD_EntityHealthPercentInfo()
+{
+	NodeName = TEXT("Entity Health Percent Info");
+}
+
+bool UBTD_EntityHealthPercentInfo::CalculateRawConditionValue(
+	UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	const AAIController* AIC = Cast<AAIController>(OwnerComp.GetOwner());
+
+	UAbilitySystemComponent* AbilitySystemComponent =
+		UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(
+			AIC->GetPawn());
+
+	if (!AbilitySystemComponent->IsValidLowLevel())
+	{
+		return false;
+	}
+
+	UHorizonAbilitySystemComponent* ASC = Cast<UHorizonAbilitySystemComponent>(
+		AbilitySystemComponent);
+
+	if (!ASC->IsValidLowLevel())
+	{
+		return false;
+	}
+
+	const UATR_BaseAttribute* AttributeSet = Cast<UATR_BaseAttribute>(
+		ASC->GetAttributeSet(UATR_BaseAttribute::StaticClass()));
+
+	UE_LOG(LogTemp, Display, TEXT("테스트: %f / %f"), AttributeSet->GetHealth()
+			, AttributeSet->GetMaxHealth());
+
+	const float RemainHealthPercent = AttributeSet->GetHealth() / AttributeSet->
+		GetMaxHealth();
+	const float ComparePercent = Percent / 100;
+
+	switch (HealthPercentCalcType)
+	{
+	case EBTD_EntityHealthPercentCalcType::Equal:
+		{
+			if (RemainHealthPercent == ComparePercent)
+			{
+				return true;
+			}
+			return false;
+		}
+	case EBTD_EntityHealthPercentCalcType::Greater:
+		{
+			if (RemainHealthPercent >= ComparePercent)
+			{
+				return true;
+			}
+			return false;
+		}
+	case EBTD_EntityHealthPercentCalcType::Less:
+		{
+			if (RemainHealthPercent <= ComparePercent)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+
+	return false;
+}
---
+++ Source/ProjectH/Ai/Decorator/BTD_EntityHealthPercentInfo.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTD_EntityHealthPercentInfo.generated.h"
+
+UENUM()
+enum class EBTD_EntityHealthPercentCalcType : uint8
+{
+	Greater, Less, Equal
+};
+
+UCLASS()
+class PROJECTH_API UBTD_EntityHealthPercentInfo : public UBTDecorator
+{
+	GENERATED_BODY()
+
+public:
+	UBTD_EntityHealthPercentInfo();
+
+protected:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp
+											, uint8* NodeMemory) const override;
+
+private:
+	UPROPERTY(EditInstanceOnly, meta = (AllowPrivateAccess = true))
+	EBTD_EntityHealthPercentCalcType HealthPercentCalcType =
+		EBTD_EntityHealthPercentCalcType::Equal;
+
+	UPROPERTY(EditInstanceOnly
+		, meta = (AllowPrivateAccess = true, ClampMin = 0, ClampMax = 100))
+	float Percent;
+};
